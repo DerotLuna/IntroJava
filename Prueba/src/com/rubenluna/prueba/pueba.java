3a1dd20841bda80b33ceacf4350cfa61e9c82d42
Ruben Luna <rubenlunamate@gmail.com>
2016-04-07 07:43:01 -0430

    Added files via upload

---
+++ Prueba/src/com/rubenluna/prueba/pueba.java
@@ -0,0 +1,37 @@
+	package com.rubenluna.prueba;
+
+public class pueba {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// final tipoDeDato = numero; para declararlo como constante
+		// long variable = numero+L; L indica que es de tipo long
+		// float variable = numeroDecimal+F; F indica que es de tipo float
+		// double no necesita agragar ninguna letra
+		// char variable = 'caracter'; se usa comillas simple
+		// se pueden declarar las varibales con $ o _ al comienzo de la variable
+		// Variables, objetos y metodos para definirlos se usar lower camel case
+		// lower camel case = variablePrueba
+		// Clases se utiliza upper camel case
+		// upper camel case = VariablePrueba
+		// Las contastes se definen en mayuscula y si es mas de una palabra se separan con _
+		// Cast: (no se puede con boolean)
+		// se utiliza en datos primitivos, instacias de una clase y tipos de objetos primitivos
+		// ejemplo de Cast
+		double d = 12.15616;
+		byte i;
+		i = (byte) d;
+		System.out.print(i);
+		// arrays
+		int conjuntoEnteros[];
+		conjuntoEnteros = new int[5]; // new crea objetos
+		// tipoDeDato array[] = new tipoDeDato[capacidad]; para definir en usa sola linea
+		// array[indicador] = valor;
+		// para mas dimesion se usa [][]...n[]
+
+		//Tipos de datos primitivas van todas las letras en minuscula
+
+		// tipos de datos objeto empiezan con mayuscula, y luego las demas en minuscula
+	}
+
+}
